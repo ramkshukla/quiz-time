54e98799bb03bd00b30636732e96e660d87ff400
ram410 <ramkumarshukla00000@gmail.com>
2020-11-27 00:44:32 +0530

    added url

--- NamePractice/urls.py
+++ NamePractice/urls.py
@@ -18,12 +18,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from myapp import views
-from django.conf.urls import patterns
+from django.conf.urls import patterns, include, url
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('', include('my_app'))
     path('', views.fun, name="fun")
 ]
 urlpatterns += patterns('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     )
